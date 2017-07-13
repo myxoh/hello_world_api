f68d2eccfd0e5f8ca31a351b821485e696a7a4ed
Nicolas Klein <myxoh10@gmail.com>
2017-07-13 21:11:47 +0100

    Generating Resources and scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :properties
+  resources :skills
+  resources :interests
+  resources :users
   get  'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
